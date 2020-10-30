4e89159932d1e950204b65785fa1f740e1ad6663
Pinois <>
2020-10-30 15:19:47 +0300

    first solution

---
+++ components/AddTopic.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function AddTopic({ addNewTopic }) {
+	return (
+		<section>
+			<header>
+				<h4>Add a topic</h4>
+			</header>
+			<article>
+				<form onSubmit={e => addNewTopic(e)} className="add-topic">
+					<input
+						required
+						type="text"
+						name="topic"
+						placeholder="Type your topic idea here"
+					/>
+					<button type="submit">Submit</button>
+				</form>
+			</article>
+		</section>
+	);
+}
